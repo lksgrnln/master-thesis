9ca7280c0289a6c6cf9eb7aec1117c13e8279b3c
Lukas Gernlein <lukas.g.gernlein@fau.de>
2024-02-07 15:21:23 +0100

    quick bugfix

--- proof_of_concept/proof_of_concept.py
+++ proof_of_concept/proof_of_concept.py
@@ -109,10 +109,10 @@ if args.model == "MLC":
 else:
     model_ximl = ResNet().to(device)
 model_ximl.load_state_dict(model.state_dict())
-# model_ximl.load_state_dict(torch.load('./models/classifier_ximl_output_11_20231226151411.pth',
+# model_ximl.load_state_dict(torch.load('./models/classifier_ximl_output_11_20240130162302.pth',
 #                                       map_location=torch.device('cpu')
 #                                       ))
-optimizer_ximl = torch.optim.Adam(model.parameters(),
+optimizer_ximl = torch.optim.Adam(model_ximl.parameters(),
                                   lr=args.learning_rate_ximl,
                                   weight_decay=args.weight_decay)
 epochs = args.epochs
@@ -132,7 +132,7 @@ if args.sequential:
     test(model, multilabel_mnist_test_loader, device, 'common_dl')
     test_explanations(model, multilabel_mnist_test_loader,
                       device, 'number of high activated pixels', 'dl')
-    train_ximl(model, epochs_ximl, optimizer_ximl,
+    train_ximl(model, epochs_ximl, optimizer,
                multilabel_mnist_train_loader, device, args.model)
     test(model, multilabel_mnist_test_loader, device, 'ximl_dl')
     test_explanations(model, multilabel_mnist_test_loader,
